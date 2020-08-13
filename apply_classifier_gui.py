bf483bbc7c004750fc5633d763e71dc9c96219b4
s-horiguchi <shuhei.horiguchi@outlook.com>
2020-08-14 00:06:05 +0900

    add apply_classifier_gui

---
+++ apply_classifier_gui.py
@@ -0,0 +1,69 @@
+from ecc.ilastik_wrapper import PixelClassifier
+import easygui
+from easygui import EgStore
+import sys, os.path
+
+
+class Settings(EgStore):
+    def __init__(self, filename="settings_apply_classifier.txt"):
+        self.ilastik_dir = "c:/Program Files/ilastik-1.3.3post3"
+        self.projfile = ""
+        self.rawimg = ""
+        self.outdir = ""
+        
+        self.filename = filename
+        self.restore()
+    
+if __name__ == "__main__":
+    settings = Settings()
+    
+    # create a new instance
+    pc = PixelClassifier()
+
+    # force it print detailed progress
+    pc.set_verbose(True)
+
+    # set ilastik path
+    settings.ilastik_dir = easygui.diropenbox(
+        title="Select ilastik path:",
+        default=settings.ilastik_dir
+    )
+    print("ilastik dir:", settings.ilastik_dir)
+    settings.store()
+    pc.set_ilastik_executable_path(settings.ilastik_dir)
+
+    # set path to ilastik project
+    settings.projfile = easygui.fileopenbox(
+        title="Select ilastik project file",
+        filetypes=["*.ilp"],
+        default=settings.projfile
+    )
+    print("project file:", settings.projfile)
+    settings.store()
+    pc.set_project_file(setings.projfile)
+
+    # Optional: control CPU and memory usage
+    #pc.set_num_threads(20)
+    #pc.set_max_memory_size(60000) # in MBs
+
+    # define input image
+    settings.rawimg = easygui.fileoepnbox(
+        title="Select raw image:",
+        filetypes=["*.hdf5", "*.h5"],
+        default=settings.rawimg
+    )
+    print("raw img:", settings.rawimg)
+    settings.store()
+    pc.set_input_image(settings.rawimg)
+
+    # define output
+    settings.outdir = easygui.diropenbox(
+        title="Select output directory:",
+        default=settings.outdir if settings.outdir else os.path.dirname(rawimg)
+    )
+    print("out dir:", settings.outdir)
+    settings.store()
+    pc.set_output_dir(settings.outdir)
+
+    # run!
+    pc.run()
--- find_objects_gui.py
+++ find_objects_gui.py
@@ -1,8 +1,33 @@
 from ecc.cellfinder import CellFinder
 import easygui
-import sys
+from easygui import EgStore
+import sys, os.path
+
+
+class Settings(EgStore):
+    def __init__(self, filename="settings_find_objects.txt"):
+        self.vx = 6.45
+        self.vy = 6.45
+        self.vz = 7.0
+        self.bsize = 120
+        self.overlap = 20
+        self.threshold = 0.70
+        self.minvol = 2
+        self.maxvol = 64
+        self.nickname = "test"
+        self.intensity_mode = "obj_mean"
+        
+        self.rawimg = ""
+        self.probimg = ""
+        self.outdir = ""
+        
+        self.filename = filename
+        self.restore()
+
 
 if __name__ == "__main__":
+    settings = Settings()
+    
     # create a new instance
     cf = CellFinder()
 
@@ -13,12 +38,15 @@ if __name__ == "__main__":
     #cf.set_num_workers(4)
 
     field_values = [
-        6.45, 6.45, 7.0, # voxel size
-        120, 20,# blocksize
-        0.70,   # threshold
-        2,      # min volume
-        64,     # max volume
-        "test", #nickname   
+        settings.vx,
+        settings.vy,
+        settings.vz,
+        settings.bsize,
+        settings.overlap,
+        settings.threshold,
+        settings.minvol,
+        settings.maxvol,
+        settings.nickname
     ]
     labels = [
         "voxel size(x)",
@@ -35,71 +63,82 @@ if __name__ == "__main__":
     msg = "Enter parameters:\n"
     err_msg = ""
     while True:
-        print(field_values)
         field_values = easygui.multenterbox(msg+err_msg, "Setting", labels, 
                                             [str(f) for f in field_values])
-        print(field_values)
+        print("values:", field_values)
         if field_values is None:
             print("Cancelled")
             sys.exit(1)
 
         err_msg = ""
         try:
-            vx = float(field_values[0])
-            vy = float(field_values[1])
-            vz = float(field_values[2])
+            settings.vx = float(field_values[0])
+            settings.vy = float(field_values[1])
+            settings.vz = float(field_values[2])
         except ValueError:
             err_msg += "voxel size must be decimal numbers\n"
             
         try:
-            bsize = int(field_values[3])
-            overlap = int(field_values[4])
+            settings.bsize = int(field_values[3])
+            settings.overlap = int(field_values[4])
         except ValueError:
             err_msg += "blocksize and overlap must be integers\n"
 
         try:
-            threshold = float(field_values[5])
+            settings.threshold = float(field_values[5])
         except ValueError:
             err_msg += "threshold must be a decimal number\n"
 
         try:
-            minvol = int(field_values[6])
-            maxvol = int(field_values[7])
+            settings.minvol = int(field_values[6])
+            settings.maxvol = int(field_values[7])
         except ValueError:
             err_msg += "min/max volume must be integers\n"
 
-        nickname = field_values[8]
+        settings.nickname = field_values[8]
         if err_msg == "":
+            settings.store()
             break
 
     # set voxel size of the input image
-    cf.set_image_voxel_size( {"X": vx, "Y": vy, "Z": vz} )
+    cf.set_image_voxel_size( {"X": settings.vx, "Y": settings.vy, "Z": settings.vz} )
 
     # Optional: set block size
-    cf.set_block_size({"blocksize": bsize, "overlap": overlap})
+    cf.set_block_size({"blocksize": settings.bsize, "overlap": settings.overlap})
 
     # set some parameters
-    cf.set_prob_threshold(threshold)
-    cf.set_min_particle_volume(minvol)
-    cf.set_max_particle_volume(maxvol)
-    cf.set_intensity_computation_mode('obj_mean')
+    cf.set_prob_threshold(settings.threshold)
+    cf.set_min_particle_volume(settings.minvol)
+    cf.set_max_particle_volume(settings.maxvol)
+    cf.set_intensity_computation_mode(settings.intensity_mode)
 
     # give it a nice nickname
-    cf.set_nickname(nickname)
+    cf.set_nickname(settings.nickname)
 
+    settings.rawimg = easygui.fileopenbox(
+        title="Select raw image:",
+        filetypes=["*.hdf5", "*.h5"],
+        default=settings.rawimg
+    )
+    settings.probimg = easygui.fileopenbox(
+        title="Select probability image:",
+        filetypes=["*.hdf5", "*.h5"],
+        default=settings.probimg
+    )
+    settings.outdir = easygui.diropenbox(
+        title="Select output directory:",
+        default=settings.outdir if settings.outdir else os.path.dirname(probimg)
+    )
 
-    outdir = easygui.diropenbox("Select output directory:")
-    rawimg = easygui.fileopenbox("Select raw image:")
-    probimg = easygui.fileopenbox("Select probability image:")
-
-    print("outdir: " + outdir)
-    print("raw img: " + rawimg)
-    print("prob img: " + probimg)
-
+    print("outdir: " + settings.outdir)
+    print("raw img: " + settings.rawimg)
+    print("prob img: " + settings.probimg)
+    settings.store()
+    
     # set output directory
-    cf.set_outdir(outdir)
-    cf.set_raw_image_path(rawimg)
-    cf.set_prob_image_path(probimg)
+    cf.set_outdir(settings.outdir)
+    cf.set_raw_image_path(settings.rawimg)
+    cf.set_prob_image_path(settings.probimg)
 
     # run!
     cf.run_main()
